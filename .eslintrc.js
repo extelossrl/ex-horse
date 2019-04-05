5bf2c8fef76b4f73873d096a4b094a1a6dbd4ee5
Giovanni Cavallo <giovannicavallo95@gmail.com>
2019-04-05 15:41:10 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ["standard", "plugin:prettier/recommended"]
-};
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ["standard", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {}
+}
