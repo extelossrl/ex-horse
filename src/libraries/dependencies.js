2036f7369a607b13b39f5ddfddd99dd50e529ab7
Giovanni Cavallo <giovannicavallo95@gmail.com>
2019-05-13 10:31:55 +0200

    add prevent duplicate subscriptions & unsubscribe method

--- src/libraries/dependencies.js
+++ src/libraries/dependencies.js
@@ -15,12 +15,35 @@ class Dependencies {
   }
 
   subscribe(aggregateName, aggregateId, service, handler) {
+    if (
+      this.subscriptions.find(
+        (sub) =>
+          sub.aggregateName === aggregateName &&
+          sub.aggregateId === aggregateId &&
+          sub.service === service &&
+          sub.handler === handler
+      )
+    ) {
+      return
+    }
+
+    const key = Math.random()
+      .toString(36)
+      .substr(2, 9)
+
     this.subscriptions.push({
+      key,
       aggregateName,
       aggregateId,
       service,
       handler
     })
+
+    return key
+  }
+
+  unsubscribe(key) {
+    this.subscriptions = this.subscriptions.filter((sub) => sub.key !== key)
   }
 }
 
