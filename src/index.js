2dcd85bb678934e7da130a8a56750d8e6816115b
Giovanni Cavallo <giovannicavallo95@gmail.com>
2019-05-13 10:34:14 +0200

    add dependencies export

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const { gql } = require("apollo-server-micro")
 const { AbilityBuilder } = require("@casl/ability")
 const Server = require("./libraries/server")
 const EventStore = require("./libraries/eventstore")
+const dependencies = require("./libraries/dependencies")
 
 module.exports = {
   gql,
   AbilityBuilder,
   Server,
-  EventStore
+  EventStore,
+  dependencies
 }
